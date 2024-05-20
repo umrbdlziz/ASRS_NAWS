ad33c272b938e7e3947612777bffbe5dddf739d0
Umar Abdul Aziz Feisal <umxziz@student.usm.my>
2024-05-20 14:23:58 +0800

    Update new project

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-import { NavBar } from './NavBar';
-import { LiveMap } from './LiveMap';
-import { TasksCard } from './TasksCard';
-
-export { NavBar, LiveMap, TasksCard };
--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-import { FleetPage } from './FleetPage';
-import { MapPage } from './MapPage';
-import { EditorPage } from './EditorPage';
-
-export { FleetPage, MapPage, EditorPage };
