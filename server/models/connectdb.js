2dc5eb95e2b90ab5b37df8be84e06ec0fe4eecfb
Umar Abdul Aziz Feisal <umxziz@student.usm.my>
2024-05-24 11:02:17 +0800

    Updated database

--- server/models/connectdb.js
+++ server/models/connectdb.js
@@ -1,29 +1,26 @@
-const mysql = require("mysql2");
-const md5 = require("md5");
+const sqlite3 = require("sqlite3").verbose();
 
-// Create a connection pool
-const con = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10, // Adjust as needed
-  queueLimit: 0,
+const DBSOURCE = process.env.DB_NAME;
+
+let db = new sqlite3.Database(DBSOURCE, (err) => {
+  if (err) {
+    // Cannot open database
+    console.error(err.message);
+    throw err;
+  }
 });
 
 // run query
 function executeRunSQL(sql, params) {
   return new Promise((resolve, reject) => {
-    con.query(sql, params, (err, result) => {
+    db.run(sql, params, function (err) {
       if (err) {
         reject(err);
-      } else {
-        resolve({
-          changes: result.affectedRows,
-          lastID: result.insertId,
-        });
       }
+      resolve({
+        changes: this.changes,
+        lastID: this.lastID,
+      });
     });
   });
 }
@@ -31,11 +28,11 @@ function executeRunSQL(sql, params) {
 // return all row
 function executeAllSQL(sql, params) {
   return new Promise((resolve, reject) => {
-    con.query(sql, params, (err, rows) => {
+    db.all(sql, params, (err, result) => {
       if (err) {
         reject(err);
       } else {
-        resolve(rows);
+        resolve(result);
       }
     });
   });
@@ -44,24 +41,18 @@ function executeAllSQL(sql, params) {
 // return one row only
 function executeGetSQL(sql, params) {
   return new Promise((resolve, reject) => {
-    con.query(sql, params, (err, rows) => {
+    db.get(sql, params, (err, result) => {
       if (err) {
         reject(err);
       } else {
-        resolve(rows[0] || null); // Return the first row or null
+        resolve(result);
       }
     });
   });
-}
-
-function createdb() {
-  console.log("Creating Tables");
-  // create tables
 }
 
 module.exports = {
   executeRunSQL,
   executeAllSQL,
   executeGetSQL,
-  createdb,
 };
--- server/server.js
+++ server/server.js
@@ -1,7 +1,63 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 5001;
+require("dotenv").config();
+
+const port = process.env.PORT;
+if (!port) {
+  console.error("Error: The PORT environment variable is not defined.");
+  process.exit(1);
+}
+
+const db = require("./models/createdb");
+
+const session = require("express-session");
+const MemoryStore = require("memorystore")(session);
+const passport = require("passport");
+// const initializePassport = require("./auth/passport-config");
+// initializePassport(
+//   passport,
+//   (username) =>
+//     db.executeGetSQL("select * from user where username = ?", [username]),
+//   (id) => db.executeGetSQL("select * from user where id = ?", [id])
+// );
+
+// app.use(
+//   session({
+//     secret: process.env.SECRET_KEY,
+//     resave: false,
+//     saveUninitialized: false,
+//     store: new MemoryStore({
+//       checkPeriod: 86400000, // prune expired entries every 24h
+//     }),
+//     cookie: {
+//       maxAge: 86400000, // Session duration in milliseconds (24h)
+//     },
+//   })
+// );
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// close database when server is shut down
+process.on("SIGINT", () => {
+  console.log("Server is shutting down...");
+
+  // Close the database connection
+  db.close((err) => {
+    if (err) {
+      console.error("Error closing database connection:", err);
+      process.exit(1);
+    } else {
+      console.log("Database connection closed.");
+      process.exit();
+    }
+  });
+});
+
+// any that are not found / default
+app.use(function (req, res) {
+  res.sendStatus(404);
+});
 
 // for production after running $npm run build in client folder
 /*
